d103dd013d5bdf3cf47e564666079b4ce1fb7a92
bgrochal <bartek.grochal@gmail.com>
2017-04-03 18:45:24 +0200

    Genotype implemented.

--- edgedetector/solver/population/genotype.py
+++ edgedetector/solver/population/genotype.py
@@ -1,7 +1,9 @@
 """
 This class represents single chromosome of an individual, which states for a solution of the problem.
 """
+import numpy as np
 
 
 class Genotype:
-    pass
+    def __init__(self, image_shape):
+        self.genes = np.zeros(image_shape, dtype=bool)
