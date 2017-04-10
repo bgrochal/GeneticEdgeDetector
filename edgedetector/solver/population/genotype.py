0dd8421f7f219a234abee80fa0c6afdfa8a47382
bgrochal <bartek.grochal@gmail.com>
2017-04-10 21:58:00 +0200

    Fitness evaluator implemented.

--- edgedetector/solver/population/genotype.py
+++ edgedetector/solver/population/genotype.py
@@ -5,5 +5,7 @@ import numpy as np
 
 
 class Genotype:
-    def __init__(self, image_shape):
+    def __init__(self, image_shape, cost, fitness=0):
         self.genes = np.zeros(image_shape, dtype=bool)
+        self.cost = cost
+        self.fitness = fitness
