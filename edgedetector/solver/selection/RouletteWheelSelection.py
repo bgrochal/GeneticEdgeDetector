8fe5b7974fb1486f59780ef34c971835a20c45b7
bgrochal <bartek.grochal@gmail.com>
2017-04-17 16:23:15 +0200

    Biased roulette wheel selection mechanism implemented.

--- edgedetector/solver/selection/RouletteWheelSelection.py
+++
@@ -1,9 +0,0 @@
-"""
-This class defines roulette wheel selection operation.
-"""
-from edgedetector.solver.selection.Selection import Selection
-
-
-class RouletteWheelSelection(Selection):
-    def select(self, population):
-        pass
