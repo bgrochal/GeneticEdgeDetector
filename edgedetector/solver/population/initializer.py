d0e72959a3acb28a41db068c39524ab722b21161
bgrochal <bartek.grochal@gmail.com>
2017-04-27 17:19:05 +0200

    Initialization based on the Robert's Cross algorithm added. Reorganization of code regarding initialization mechanism.

--- edgedetector/solver/population/initializer.py
+++ edgedetector/solver/population/initializer.py
@@ -1,12 +1,33 @@
 """
 This class creates a population of individuals (so called gene pool).
 """
+
+import sys
 from abc import ABC, abstractmethod
+from random import random
+
+from edgedetector.solver.population.genotype import Genotype
 
 
 class Initializer(ABC):
+    def __init__(self, population_size, genotype_shape, similarity, threshold):
+        self.population_size = population_size
+        self.genotype_shape = genotype_shape
+        self.similarity = similarity
+        self.threshold = threshold
+
     @abstractmethod
     def initialize(self):
         # TODO: Should we prevent creating two initial individuals with exactly the same genotype?
         # TODO: Should we also ensure with 100% probability that not all generated genotypes are exactly the same?
         raise NotImplementedError
+
+    def _generate_random_genotype(self, reference_image):
+        genotype = Genotype(self.genotype_shape, sys.maxsize)
+        for i in range(genotype.genes.shape[0]):
+            for j in range(genotype.genes.shape[1]):
+                if random() <= self.similarity:
+                    genotype.genes[i][j] = True if reference_image[i][j] >= self.threshold else False
+                else:
+                    genotype.genes[i][j] = 0
+        return genotype
--- test/solver/population/random_initializer_test.py
+++ test/solver/population/random_initializer_test.py
@@ -11,13 +11,14 @@ from edgedetector.solver.population.random_initializer import RandomInitializer
 class RandomInitializerTest(TestCase):
     def __init__(self, methodName='runTest'):
         super().__init__(methodName)
+        self.population_size = 5
         self.shape = (10, 10)
-        self.initializer = RandomInitializer(self.shape, 5, 0.1, 0.8, np.zeros(self.shape))
+        self.initializer = RandomInitializer(self.population_size, np.zeros(self.shape), 0.8, 0.1, np.zeros(self.shape))
 
     def test_initialize(self):
         population = self.initializer.initialize()
         self.assertIsInstance(population, list)
-        self.assertEqual(len(population), 5)
+        self.assertEqual(len(population), self.population_size)
 
         for individual in population:
-            self.assertEqual(individual.genes.shape, (10, 10))
+            self.assertEqual(individual.genes.shape, self.shape)
